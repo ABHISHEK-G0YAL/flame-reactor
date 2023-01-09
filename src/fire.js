cd10f6c8097cea2a94c79ef3d05c5501cfe12674
Abhishek Goyal <abhishek252167@gmail.com>
2023-01-10 01:45:30 +0530

    add bonfire

---
+++ src/fire.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './fire.css'
+
+class Fire extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false,
+    };
+  }
+
+  toggleFire = () => {
+    this.setState(prevState => ({ on: !prevState.on }));
+  };
+
+  render() {
+    return (
+      <div className="fire">
+        {
+          this.state.on &&
+          <div className="flames">
+            <div className="flame"></div>
+            <div className="flame"></div>
+            <div className="flame"></div>
+            <div className="flame"></div>
+          </div>
+        }
+        <div className="logs" onClick={this.toggleFire}></div>
+      </div>
+    );
+  }
+}
+
+export default Fire;
